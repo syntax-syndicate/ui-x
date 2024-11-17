920a2ea427a60d2370bf7ac067af732024d1af23
junwen-k <40173716+junwen-k@users.noreply.github.com>
2024-11-17 20:50:08 +0800

    chore(site): update site config

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,12 +1,12 @@
 export const siteConfig = {
-  name: "shadcn/ui",
-  url: "https://ui.shadcn.com",
+  name: "junwen-k/ui-x",
+  url: "https://ui-x.junwen-k.dev",
   ogImage: "https://ui.shadcn.com/og.jpg",
   description:
     "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
   links: {
     twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn-ui/ui",
+    github: "https://github.com/junwen-k/ui-x",
   },
 }
 
