25ddb120a635901ce16c9cbe9549e721c34cfbba
junwen-k <40173716+junwen-k@users.noreply.github.com>
2025-01-05 23:09:20 +0800

    fix(site): update site url

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,13 +1,14 @@
 export const siteConfig = {
   name: "junwen-k/ui-x",
   url: "https://ui-x.junwen-k.dev",
-  ogImage: "https://ui.shadcn.com/og.jpg",
+  ogImage: "https://ui-x.junwen-k.dev/og.jpg",
   description:
     "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/junwen-k/ui-x",
+    github: "https://github.com/junwen-k",
+    githubUiX: "https://github.com/junwen-k/ui-x",
+    githubShadcnUi: "https://github.com/shadcn-ui/ui",
   },
-}
+} as const
 
 export type SiteConfig = typeof siteConfig
